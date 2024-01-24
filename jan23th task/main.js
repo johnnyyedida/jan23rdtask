f9d3e409219e7fa83b8e67fe8bd6fe8d1f11f15c
johnnyyedida <156890587+johnnyyedida@users.noreply.github.com>
2024-01-24 14:22:43 +0530

    Add files via upload

---
+++ jan23th task/main.js
@@ -0,0 +1,12 @@
+var score= 0;
+
+function decrease() {
+    score=score-10;
+    document.getElementById("box").textContent=score;
+    
+}
+
+function increase() {
+    score=score+10;
+    document.getElementById("box").textContent=score;
+}
